2977f278cfd4a11204b5a20c91d2003fab920a45
benFellows-229 <115678431+benFellows-229@users.noreply.github.com>
2024-09-25 12:10:40 -0700

    Delete v0_app/my-osint-app directory

--- v0_app/my-osint-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import OsintSearch from './osint-search'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100 py-12">
-      <OsintSearch />
-    </main>
-  )
-}
